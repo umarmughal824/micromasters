aec449e563af75a4c8742eb3cf42fe2acee38c95
George Schneeloch <gschneel@mit.edu>
2016-03-02 17:02:37 -0500

    Initial setup for micromasters portal

---
+++ ui/models.py
@@ -0,0 +1,6 @@
+"""
+Models classes needed for ui
+"""
+# from django.db import models
+
+# Create your models here.
---
+++ ui/views.py
@@ -0,0 +1,6 @@
+"""
+ui views
+"""
+# from django.shortcuts import render
+
+# Create your views here.
