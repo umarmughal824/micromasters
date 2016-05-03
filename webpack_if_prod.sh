2e6bd46a5b60fe967c0a6446d7c5c84a01e6f6fd
George Schneeloch <gschneel@mit.edu>
2016-05-03 13:56:14 -0400

    Moved stage1 plugin into production. Add --bail to exit with non-zero status on error

--- webpack_if_prod.sh
+++ webpack_if_prod.sh
@@ -2,5 +2,5 @@
 set -euf -o pipefail
 if [[ "$NODE_ENV" != "development" ]]
 then
-    node node_modules/webpack/bin/webpack.js --config webpack.config.prod.js
+    node node_modules/webpack/bin/webpack.js --config webpack.config.prod.js --bail
 fi
