603bb14bed50c3373f10d7273b6c46044eb1b178
Giovanni Di Milia <gdimilia@mit.edu>
2016-08-09 16:38:13 -0400

    Added Search REST API

---
+++ roles/api.py
@@ -0,0 +1,23 @@
+"""
+API for roles
+"""
+from rolepermissions.verifications import has_object_permission
+
+from roles.models import Role
+
+
+def get_advance_searchable_programs(user):
+    """
+    Helper function to retrieve all the programs where the user is allowed to search
+
+    Args:
+        user (User): Django user instance
+    Returns:
+        list: list of courses.models.Program instances
+    """
+    user_role_program = Role.objects.filter(user=user)
+    programs = [
+        role.program for role in user_role_program
+        if has_object_permission('can_advance_search', user, role.program)
+    ]
+    return programs
---
+++ search/views.py
@@ -0,0 +1,85 @@
+"""
+Views for the Search app
+"""
+
+import logging
+
+from django.conf import settings
+from elasticsearch_dsl import Search, Q
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from roles.api import get_advance_searchable_programs
+from search.api import (
+    DOC_TYPES,
+    get_conn
+)
+
+log = logging.getLogger(__name__)
+
+
+class ElasticProxyView(APIView):
+    """
+    Elasticsearch proxy needed to enforce authentication and permissions
+    """
+    authentication_classes = (SessionAuthentication, )
+    permission_classes = (IsAuthenticated, )
+
+    def dispatch(self, request, *args, **kwargs):
+        # import pdb
+        # pdb.set_trace()
+        return APIView.dispatch(self, request, *args, **kwargs)
+
+    def _search_elasicsearch(self, request):
+        """
+        Common function that will take care of handling requests coming from different methods.
+        """
+        # make sure there is a live connection
+        get_conn()
+
+        # create a search object and load the query coming from the client
+        search = Search(index=settings.ELASTICSEARCH_INDEX, doc_type=DOC_TYPES)
+        search = search.from_dict(request.data)
+
+        # extract all the programs where the user is allowed to search
+        users_allowed_programs = get_advance_searchable_programs(request.user)
+        # if the user cannot search any program, return an error
+        if not users_allowed_programs:
+            return Response(
+                status=status.HTTP_403_FORBIDDEN,
+                data={'error': 'no_available_programs'}
+            )
+        # no matter what the query is, limit the programs to the allowed ones
+        # if this is a superset of what searchkit sends, this will not impact the result
+        query_limit = Q(
+            'nested',
+            path="program",
+            query=Q(
+                'bool',
+                should=[
+                    Q('term', **{'program.id': program.id}) for program in users_allowed_programs
+                ]
+            ),
+        )
+        search = search.query(query_limit)
+        # execute the query
+        results = search.execute()
+        # return the result as a dictionary
+        return Response(
+            results.to_dict()
+        )
+
+    def get(self, request, *args, **kwargs):
+        """
+        Handler for GET requests
+        """
+        return self._search_elasicsearch(request)
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handler for POST requests
+        """
+        return self._search_elasicsearch(request)
