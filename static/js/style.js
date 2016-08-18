c60a655c6ecd901b6275a6fc231f8a37dd25e937
George Schneeloch <gschneel@mit.edu>
2016-08-18 15:25:12 -0400

    Moved material-design-lite CSS import before our CSS imports (#886)

--- static/js/style.js
+++ static/js/style.js
@@ -1,2 +1,8 @@
 __webpack_public_path__ = `http://${SETTINGS.host}:8078/`;  // eslint-disable-line no-undef, camelcase
+// react-mdl material-design-lite file
+import 'style!css!react-mdl/extra/material.css';
+// react-virtualized requirement
+import 'style!css!react-virtualized/styles.css';
+
+// This should come last to override other styles
 import '../scss/layout.scss';
