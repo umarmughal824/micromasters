c39a0939c660cd0525e5ac8f82c7be436391e9b2
Giovanni Di Milia <gdimilia@mit.edu>
2016-08-30 15:37:34 -0400

    Added REST API for user enrolled programs

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,8 @@
 """Views for courses"""
 from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+
 from courses.models import CourseRun, Program
 from courses.serializers import CourseRunSerializer, ProgramSerializer
 
@@ -14,3 +17,19 @@ class CourseRunViewSet(viewsets.ReadOnlyModelViewSet):
     """API for the Program collection"""
     queryset = CourseRun.objects.filter(course__program__live=True)
     serializer_class = CourseRunSerializer
+
+
+class ProgramEnrollmentListView(ListAPIView):
+    """API for the User Program Enrollments"""
+
+    serializer_class = ProgramSerializer
+    permission_classes = (
+        IsAuthenticated,
+    )
+
+    def get_queryset(self):
+        """
+        Filter coursed by repository
+        """
+        queryset = Program.objects.filter(programenrollment__user=self.request.user)
+        return queryset.order_by('title')
