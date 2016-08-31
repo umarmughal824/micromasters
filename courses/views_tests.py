ba7ebe3cce90f33174fd01878d7e1df28afac2dc
Giovanni Di Milia <gdimilia@mit.edu>
2016-08-31 12:10:00 -0400

    Added tests

--- courses/views_tests.py
+++ courses/views_tests.py
@@ -9,8 +9,10 @@ from rest_framework.test import APITestCase
 from courses.factories import ProgramFactory, CourseFactory
 from courses.serializers import ProgramSerializer
 from dashboard.factories import ProgramEnrollmentFactory
+from dashboard.models import ProgramEnrollment
 from profiles.factories import UserFactory
 from search.base import ESTestCase
+from rest_framework.status import HTTP_409_CONFLICT
 
 
 class ProgramTests(ESTestCase):
@@ -74,15 +76,30 @@ class ProgramEnrollmentTests(ESTestCase, APITestCase):
                 user=cls.user1,
                 program=program,
             )
-        cls.url = reverse('user_program_enrollment')
+        cls.url = reverse('user_program_enrollments')
+
+    def setUp(self):
+        super(ProgramEnrollmentTests, self).setUp()
+        for program in (self.program1, self.program2,):
+            ProgramEnrollmentFactory(
+                user=self.user1,
+                program=program,
+            )
+        self.client.force_login(self.user1)
+
+    def tearDown(self):
+        super(ProgramEnrollmentTests, self).tearDown()
+        ProgramEnrollment.objects.all().delete()
 
     def test_anonymous(self):
         """Anonymous user cannot access the endpoint"""
+        self.client.logout()
         resp = self.client.get(self.url)
         assert resp.status_code == status.HTTP_403_FORBIDDEN
 
     def test_no_enrollments(self):
         """Requests for a user with no program enrollments result in an empty list"""
+        self.client.logout()
         self.client.force_login(self.user2)
         resp = self.client.get(self.url)
         assert resp.status_code == status.HTTP_200_OK
@@ -90,14 +107,42 @@ class ProgramEnrollmentTests(ESTestCase, APITestCase):
 
     def test_enrollments(self):
         """Only the programs where the user is enrolled in are returned"""
-        self.client.force_login(self.user1)
         resp = self.client.get(self.url)
         assert resp.status_code == status.HTTP_200_OK
-        expected_data = sorted(
-            [
-                OrderedDict(sorted(program.data.items(), key=lambda t: t[0]))
-                for program in (ProgramSerializer(self.program1), ProgramSerializer(self.program2),)
-            ],
-            key=lambda x: x['title'],
-        )
-        assert resp.data == expected_data
+        expected_enrollments_ids = [
+            program.pk for program in (self.program1, self.program2,)
+        ]
+        for enr in resp.data:
+            assert enr.get('id') in expected_enrollments_ids
+
+    def test_crete_no_program_id(self):
+        """Missing mandatory program_id parameter"""
+        resp = self.client.post(self.url, {}, format='json')
+        assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_crete_invalid_program_id(self):
+        """program_id parameter must be an integer"""
+        resp = self.client.post(self.url, {'program_id': 'foo'}, format='json')
+        assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_crete_enrollment_exists(self):
+        """Test in case the enrollment is already there"""
+        resp = self.client.post(self.url, {'program_id': self.program1.pk}, format='json')
+        assert resp.status_code == status.HTTP_409_CONFLICT
+
+    def test_crete_program_does_not_exists(self):
+        """Test in case the program does not exist"""
+        resp = self.client.post(self.url, {'program_id': 1234567}, format='json')
+        assert resp.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_crete_program_not_live(self):
+        """Test in case the program is not live"""
+        program = ProgramFactory.create(live=False)
+        resp = self.client.post(self.url, {'program_id': program.pk}, format='json')
+        assert resp.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_crete(self):
+        """Test for happy path"""
+        resp = self.client.post(self.url, {'program_id': self.program3.pk}, format='json')
+        assert resp.status_code == status.HTTP_200_OK
+        assert resp.data.get('id') == self.program3.pk
