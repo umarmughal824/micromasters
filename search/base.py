615250a4d8e46f7bbdbe00f66ab0a112f8df0f8a
George Schneeloch <gschneel@mit.edu>
2016-09-01 13:37:56 -0400

    Added test case to mock out elasticsearch (#902)

--- search/base.py
+++ search/base.py
@@ -1,7 +1,12 @@
 """
 Base test classes for search
 """
-from django.test import TestCase
+from mock import patch
+
+from django.test import (
+    override_settings,
+    TestCase,
+)
 
 from search.indexing_api import recreate_index
 
@@ -14,3 +19,32 @@ class ESTestCase(TestCase):
     def setUp(self):
         super(ESTestCase, self).setUp()
         recreate_index()
+
+
+@override_settings(ELASTICSEARCH_URL="fake")
+class MockedESTestCase(TestCase):
+    """
+    Mock ES signals
+    """
+
+    def setUp(self):
+        self.patchers = [
+            patch('search.api.get_conn', autospec=True),
+            patch('search.api.bulk', autospec=True, return_value=(0, [])),
+            patch('search.api.Mapping', autospec=True),
+        ]
+        for patcher in self.patchers:
+            patcher.start()
+        try:
+            super(MockedESTestCase, self).setUp()
+        except:
+            for patcher in self.patchers:
+                patcher.stop()
+            raise
+
+    def tearDown(self):
+        try:
+            super(MockedESTestCase, self).tearDown()
+        finally:
+            for patcher in self.patchers:
+                patcher.stop()
