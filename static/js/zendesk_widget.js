67b3e9ba78e443e0835b81a8c06c29a9f57c3afd
Amir Qayyum Khan <amir.qayyum.khan@gmail.com>
2016-10-08 15:32:11 +0500

    Removed zendesk widget from homepage

---
+++ static/js/zendesk_widget.js
@@ -0,0 +1,30 @@
+/* global SETTINGS:false */
+__webpack_public_path__ = `http://${SETTINGS.host}:8078/`;  // eslint-disable-line no-undef, camelcase
+
+// Start of odl Zendesk Widget script
+/*<![CDATA[*/
+window.zEmbed || function (e, t) {
+  let n, o, d, i, s, a = [],
+    r = document.createElement("iframe");
+  window.zEmbed = function () {
+    a.push(arguments);
+  },
+    window.zE = window.zE || window.zEmbed, r.src = "javascript:false", r.title = "",
+    r.role = "presentation", (r.frameElement || r).style.cssText = "display: none",
+    d = document.getElementsByTagName("script"), d = d[d.length - 1],
+    d.parentNode.insertBefore(r, d), i = r.contentWindow, s = i.document;
+  try {
+    o = s;
+  } catch (e) {
+    n = document.domain,
+    r.src = `javascript:var d=document.open();d.domain="${n}";void(0);`,
+    o = s;
+  }
+  o.open()._l = function () {
+    const o = this.createElement("script");
+    n && (this.domain = n), o.id = "js-iframe-async", o.src = e, this.t = +new Date, this.zendeskHost = t,
+      this.zEQueue = a, this.body.appendChild(o);
+  }, o.write('<body onload="document._l();">'),
+    o.close();
+}("https://assets.zendesk.com/embeddable_framework/main.js", "odl.zendesk.com");
+ /*]]>*/
