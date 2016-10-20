26ac4b038a1fcfa704d5c30304ebb6f758440df8
Amir Qayyum Khan <amir-qayyum-khan@users.noreply.github.com>
2016-10-20 20:55:23 +0500

    hid facets when they have no hits (#1407)

--- static/js/components/search/FilterVisibilityToggle.js
+++ static/js/components/search/FilterVisibilityToggle.js
@@ -1,8 +1,10 @@
 // @flow
 import React from 'react';
+import _ from 'lodash';
+import { SearchkitComponent } from 'searchkit';
 import Icon from 'react-mdl/lib/Icon';
 
-export default class FilterVisibilityToggle extends React.Component {
+export default class FilterVisibilityToggle extends SearchkitComponent {
   props: {
     filterName:             string,
     checkFilterVisibility:  (filterName: string) => boolean,
@@ -15,13 +17,29 @@ export default class FilterVisibilityToggle extends React.Component {
     return checkFilterVisibility(filterName) ? "" : "closed";
   };
 
-  openStateIcon: Function = (): React$Element<*> => (
-    <Icon
+  isInResults: Function = (id: string): boolean => {
+    if (this.getResults()) {
+      const elmId = (id === "birth_location") ? "profile.birth_country3" : id;
+      const docCount = _.get(this.getResults(), ['aggregations', elmId, 'doc_count'], 0);
+
+      if (docCount > 0) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  openStateIcon: Function = (children: React$Element<*>): React$Element<*>|null => {
+    if (!this.isInResults(children.props.id)) {
+      return null;
+    }
+
+    return <Icon
       name="arrow_drop_down"
       onClick={this.toggleFilterVisibility}
       className={this.openClass()}
-    />
-  );
+    />;
+  }
 
   toggleFilterVisibility: Function = (): void => {
     const {
@@ -39,7 +57,7 @@ export default class FilterVisibilityToggle extends React.Component {
     const { children } = this.props;
     return (
       <div className={`filter-visibility-toggle ${this.openClass()}`}>
-        { this.openStateIcon() }
+        { this.openStateIcon(children) }
         { children }
       </div>
     );
