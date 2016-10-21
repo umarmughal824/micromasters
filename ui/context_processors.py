110b0cb72dad09716cf4e80d227feba99913f5aa
annagav <annagav@mit.edu>
2016-10-21 16:10:06 -0400

    Add Smartlook tracking

--- ui/context_processors.py
+++ ui/context_processors.py
@@ -8,3 +8,9 @@ def google_analytics(request):
     """inject GA_TRACKING_ID into templates"""
     # pylint: disable=unused-argument
     return {'GA_TRACKING_ID': settings.GA_TRACKING_ID}
+
+
+def smartlook(request):
+    """inject SL_TRACKING_ID into templates"""
+    # pylint: disable=unused-argument
+    return {'SL_TRACKING_ID': settings.SL_TRACKING_ID}
