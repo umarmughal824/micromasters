36a1d54a0dbf10141b319817514e282141213bfa
Giovanni Di Milia <gdimilia@mit.edu>
2016-10-25 15:43:17 -0400

    removed list of panels

--- micromasters/settings.py
+++ micromasters/settings.py
@@ -507,17 +507,6 @@ if DEBUG:
         'debug_toolbar.middleware.DebugToolbarMiddleware',
     ) + MIDDLEWARE_CLASSES
 
-    DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar.panels.versions.VersionsPanel',
-        'debug_toolbar.panels.timer.TimerPanel',
-        'debug_toolbar.panels.settings.SettingsPanel',
-        'debug_toolbar.panels.headers.HeadersPanel',
-        'debug_toolbar.panels.request.RequestPanel',
-        'debug_toolbar.panels.sql.SQLPanel',
-        'debug_toolbar.panels.signals.SignalsPanel',
-        'debug_toolbar.panels.logging.LoggingPanel',
-    )
-
     def show_toolbar(request):
         """
         Custom function needed because of bug in wagtail.
