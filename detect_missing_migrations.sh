7e7351f2cc27f0ea135b499ba7f6f0a0394c8124
George Schneeloch <gschneel@mit.edu>
2016-10-27 14:15:39 -0400

    Fixed detect_missing_migrations.sh (#1583)

--- detect_missing_migrations.sh
+++ detect_missing_migrations.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-set -e -o pipefail
 
-if ./manage.py migrate --fake --no-input | grep "Your models have changes that are not yet reflected" > /dev/null
+TMPFILE=$(mktemp)
+./manage.py migrate --no-input >& "$TMPFILE"
+if cat "$TMPFILE" | grep "Your models have changes that are not yet reflected" > /dev/null
 then
     echo "Error: one or more migrations are missing:"
     echo
-    ./manage.py migrate --fake --no-input
+    cat "$TMPFILE"
+    rm "$TMPFILE"
     exit 1
+else
+    rm "$TMPFILE"
 fi
