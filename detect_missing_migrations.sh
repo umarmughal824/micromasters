54bdc1b6f4238e4733e9254f7c88392d95e37154
George Schneeloch <gschneel@mit.edu>
2016-10-27 10:27:16 -0400

    Protected detect_missing_migrations.sh against hanging for console input (#1573)

--- detect_missing_migrations.sh
+++ detect_missing_migrations.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e -o pipefail
 
-if ./manage.py migrate --fake | grep "Your models have changes that are not yet reflected" > /dev/null
+if ./manage.py migrate --fake --no-input | grep "Your models have changes that are not yet reflected" > /dev/null
 then
     echo "Error: one or more migrations are missing:"
     echo
-    ./manage.py migrate --fake
+    ./manage.py migrate --fake --no-input
     exit 1
 fi
