4230b9731f4783c44b6eb94266410d7e6ecf9e43
George Schneeloch <gschneel@mit.edu>
2016-11-04 17:06:35 -0400

    Added country_of_residence to FinancialAid model (#1650)

--- financialaid/models.py
+++ financialaid/models.py
@@ -75,6 +75,7 @@ class FinancialAid(TimestampedModel, AuditableModel):
     date_exchange_rate = models.DateTimeField(null=True)
     date_documents_sent = models.DateField(null=True, blank=True)
     justification = models.TextField(null=True)
+    country_of_residence = models.TextField()
 
     def save(self, *args, **kwargs):
         """
