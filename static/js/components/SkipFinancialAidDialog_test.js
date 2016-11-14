a23fc8f5f33f3a85426657004e46b778225a5767
Alice Pote <alice.writes.wrongs@gmail.com>
2016-11-14 14:13:10 -0500

    Added some new tests for financial aid frontend code
    We wanted to get the test coverage up a bit.
    
    part of #1575

---
+++ static/js/components/SkipFinancialAidDialog_test.js
@@ -0,0 +1,55 @@
+// @flow
+import React from 'react';
+import { mount } from 'enzyme';
+import { assert } from 'chai';
+import sinon from 'sinon';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import TestUtils from 'react-addons-test-utils';
+
+import SkipFinancialAidDialog from './SkipFinancialAidDialog';
+
+describe('SkipFinancialAidDialog', () => {
+  let sandbox;
+  let cancelStub, skipStub;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    cancelStub = sandbox.stub();
+    skipStub = sandbox.stub();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  let renderDialog = (open=true) => {
+    mount(
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <SkipFinancialAidDialog
+          open={open}
+          cancel={cancelStub}
+          skip={skipStub}
+          fullPrice={<span>'$3000'</span>}
+        />
+      </MuiThemeProvider>
+    );
+    return document.querySelector('.skip-financial-aid-dialog');
+  };
+
+  it('should have some text and a title, including the full price amount', () => {
+    let dialogText = renderDialog().textContent;
+    assert.include(dialogText, 'Are you sure?');
+    assert.include(dialogText, '$3000');
+  });
+
+  it('should have a confirm button', () => {
+    TestUtils.Simulate.click(renderDialog().querySelector('.save-button'));
+    assert.ok(skipStub.called, 'skip function should have been called');
+  });
+
+  it('should have a cancel button', () => {
+    TestUtils.Simulate.click(renderDialog().querySelector('.cancel-button'));
+    assert.ok(cancelStub.called, 'cancel function should have been called');
+  });
+});
