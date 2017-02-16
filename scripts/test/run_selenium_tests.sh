5b22213311942f06214813645f9385f582db031c
George Schneeloch <gschneel@mit.edu>
2017-02-16 11:22:34 -0500

    Make separate selenium container for tests (#2634)
    * Don't link web container to grid
    
    * Link redis to selenium container
    
    * Use python image
    
    * Don't run python container

--- scripts/test/run_selenium_tests.sh
+++ scripts/test/run_selenium_tests.sh
@@ -8,4 +8,4 @@ then
     echo "webpack-stats.json must exist before running the selenium tests. Run webpack to create it."
     exit 1
 fi
-docker-compose run -e DEBUG=False -e DJANGO_LIVE_TEST_SERVER_ADDRESS=0.0.0.0:8286 web py.test ./selenium_tests
+docker-compose run -e DEBUG=False -e DJANGO_LIVE_TEST_SERVER_ADDRESS=0.0.0.0:8286 selenium py.test ./selenium_tests
