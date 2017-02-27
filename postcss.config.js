12f84133a048d1e7b91aea9becf7a9ad6f02cefb
David Baumgold <david@davidbaumgold.com>
2017-02-27 15:28:12 -0500

    Load SCSS-referenced URLs via Cloudfront on production

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,20 @@
+const autoprefixer = require('autoprefixer');
+const assets = require('postcss-assets');
+
+const CF_DIST = process.env.CLOUDFRONT_DIST;
+let baseUrl = '/';
+if (CF_DIST) {
+  baseUrl = `https://${CF_DIST}.cloudfront.net/`;
+}
+
 module.exports = {
   plugins: [
-    require('autoprefixer')({
+    autoprefixer({
       browsers: ['> 1%']
+    }),
+    assets({
+      baseUrl: baseUrl,
+      loadPaths: ['static']
     })
   ]
 }
