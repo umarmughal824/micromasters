5b027e3c71d235bff61036857e2e74d93bb787d5
Alice Pote <alice.writes.wrongs@gmail.com>
2017-04-25 15:15:29 -0400

    Release 0.56.1

--- micromasters/settings.py
+++ micromasters/settings.py
@@ -22,7 +22,7 @@ from celery.schedules import crontab
 from django.core.exceptions import ImproperlyConfigured
 
 
-VERSION = "0.56.0"
+VERSION = "0.56.1"
 
 CONFIG_PATHS = [
     os.environ.get('MICROMASTERS_CONFIG', ''),
