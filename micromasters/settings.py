53de67ad43a644cb2a707e427b959b99cd5d2ea9
Gavin Sidebottom <gsidebo@mit.edu>
2017-05-08 16:57:07 -0400

    Release 0.57.7

--- micromasters/settings.py
+++ micromasters/settings.py
@@ -22,7 +22,7 @@ from celery.schedules import crontab
 from django.core.exceptions import ImproperlyConfigured
 
 
-VERSION = "0.57.6"
+VERSION = "0.57.7"
 
 CONFIG_PATHS = [
     os.environ.get('MICROMASTERS_CONFIG', ''),
