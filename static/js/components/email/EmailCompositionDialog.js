umarmughal824/micromasters

commit 74f0ea25d1b45cd16d2aa77e049b1dd8ada097c7

Mail: Add Recipient Variables Toolbar (#3145)

static/js/components/email/EmailCompositionDialog.js
@@ -4,9 +4,10 @@ import Dialog from 'material-ui/Dialog';
 import R from 'ramda';
 import { Editor } from 'react-draft-wysiwyg';
 // $FlowFixMe: Flow thinks this module isn't present for some reason
-import { EditorState, ContentState, convertFromHTML } from 'draft-js';
+import { EditorState, ContentState, convertFromHTML, Modifier } from 'draft-js';
 
 import AutomaticEmailOptions from './AutomaticEmailOptions';
+import RecipientVariableButton from './RecipientVariableButton';
 import { FETCH_PROCESSING } from '../../actions';
 import { dialogActions } from '../inputs/util';
 import { isNilOrBlank } from '../../util/util';
@@ -32,7 +33,7 @@ const convertHTMLToEditorState = (html: string): Object => {
 // Otherwise, we set our state to be a blank object, which will cause the
 // <Editor /> component to create a blank EditorState of its own accord.
 const editorStateFromProps = R.compose(
-  S.maybe({}, R.objOf('editorState')),
+  S.maybe({editorState: EditorState.createEmpty()}, R.objOf('editorState')),
   S.map(convertHTMLToEditorState),
   S.filter(R.compose(R.not, R.isEmpty)),
   getm('body'),
@@ -70,7 +71,7 @@ type EmailDialogProps = {
 
 export default class EmailCompositionDialog extends React.Component {
   state: {
-    editorState: ?Object,
+    editorState: Object,
   };
 
   props: EmailDialogProps;
@@ -85,6 +86,40 @@ export default class EmailCompositionDialog extends React.Component {
       this.setState(editorStateFromProps(nextProps));
     }
   }
+
+  insertRecipientVariable = (variableName: string) => {
+
+    const {editorState} = this.state;
+    let selection = editorState.getSelection();
+    const entityKey = editorState
+      .getCurrentContent()
+      .createEntity('MENTION', 'IMMUTABLE', {})
+      .getLastCreatedEntityKey();
+
+    let contentState = Modifier.replaceText(
+      editorState.getCurrentContent(),
+      selection,
+      `${variableName}`,
+      editorState.getCurrentInlineStyle(),
+      entityKey,
+    );
+    let newEditorState = EditorState.push(editorState, contentState, 'insert-characters');
+    // insert a blank space after the variable
+    selection = newEditorState.getSelection().merge({
+      anchorOffset: selection.get('anchorOffset') + variableName.length,
+      focusOffset: selection.get('anchorOffset') + variableName.length,
+    });
+    newEditorState = EditorState.acceptSelection(newEditorState, selection);
+    contentState = Modifier.insertText(
+      newEditorState.getCurrentContent(),
+      selection,
+      ' ',
+      newEditorState.getCurrentInlineStyle(),
+      undefined,
+    );
+    this.onEditorStateChange(EditorState.push(newEditorState, contentState, 'insert-characters'));
+
+  };
 
   showValidationError = (fieldName: string): ?React$Element<*> => {
     const { activeEmail: { validationErrors } } = this.props;
@@ -133,7 +168,7 @@ export default class EmailCompositionDialog extends React.Component {
   };
 
   clearEditorState = () => {
-    this.setState({ editorState: null });
+    this.setState({ editorState: EditorState.createEmpty() });
   };
 
   closeEmailComposerAndSend = () => {
@@ -146,6 +181,15 @@ export default class EmailCompositionDialog extends React.Component {
     closeAndClearEmailComposer();
     this.clearEditorState();
   };
+
+  makeCustomToolbarButtons = R.map((variableName: string) => {
+    return <RecipientVariableButton
+      value={variableName}
+      key={variableName}
+      onClick={()=>(this.insertRecipientVariable(`[${variableName}]`))}
+    />;
+  });
+
 
   render() {
     if (!this.props.activeEmail) return null;
@@ -193,6 +237,10 @@ export default class EmailCompositionDialog extends React.Component {
           onEditorStateChange={this.onEditorStateChange}
           toolbar={draftWysiwygToolbar}
         />
+        <div className="toolbar-below">
+          <div className="insert" >Insert:</div>
+          {this.makeCustomToolbarButtons(['PreferredName', 'Email'])}
+        </div>
         { this.showValidationError('body') }
       </div>
     </Dialog>;

static/js/components/email/EmailCompositionDialog_test.js
@@ -190,6 +190,13 @@ describe('EmailCompositionDialog', () => {
       assert.equal(link.textContent, 'A Link!');
       assert.equal(link.href, "https://en.wikipedia.org/wiki/Potato");
     });
+
+    it('should insert recipient variables', () => {
+      renderDialog();
+      TestUtils.Simulate.click(getDialog().querySelector('.button-Email'));
+      assert.include(getEditorContents().textContent, '[Email]');
+
+    });
   });
 
   it('should render recipients', () => {

static/js/components/email/RecipientVariableButton.js
@@ -1,0 +1,20 @@
+// @flow
+import React from 'react';
+
+export default class RecipientVariableButton extends React.Component {
+
+  props: {
+    onClick: Function,
+    value: string
+  };
+
+  render() {
+    const { onClick, value } = this.props;
+    return <div
+      className={`rdw-option-wrapper custom-toolbar-button button-${value}`}
+      onClick={onClick}
+    >
+      {`[${value}]`}
+    </div>;
+  }
+}

static/scss/email-composition-dialog.scss
@@ -37,6 +37,7 @@
 
         .rdw-editor-main {
           min-height: 250px;
+          height: auto;
           border: 1px solid $border-color;
           border-radius: 2px;
 
@@ -167,5 +168,21 @@
         color: $darkgray;
       }
     }
+  }
+}
+
+.toolbar-below {
+  padding: 6px 5px 0;
+  display: flex;
+
+  .custom-toolbar-button {
+    width: 130px;
+    height: 30px;
+  }
+
+  .insert {
+    padding: 5px;
+    height: 30px;
+    margin: 0 4px;
   }
 }

commit 683237323067cf3f8888dfbec384cc6ef81ad9cb

Fixed email composiition dialog body loading
This fixes a bug that was introduced in #3145 (mail recipient
variables). In that PR we changed some aspects of how we are managing
the draft.js `EditorState` object, and, in particular, we stopped using
`this.state.editorState === null` to indicate an 'empty' state. Instead,
we started setting it to `EditorState.createEmpty()`, but we did not
update a check (with `R.isNil`) which decided whether a new value needed
to be loaded for the body. This fixes that issue, by getting the
`ContentState` object and calling the `hasText` method on it to
differentiate an empty from a hydrated state.

closes #3181
pr #3185

static/js/components/email/EmailCompositionDialog.js
@@ -82,7 +82,7 @@ export default class EmailCompositionDialog extends React.Component {
   }
 
   componentWillReceiveProps (nextProps: EmailDialogProps) {
-    if (R.isNil(this.state.editorState)) {
+    if (!this.state.editorState.getCurrentContent().hasText()) {
       this.setState(editorStateFromProps(nextProps));
     }
   }