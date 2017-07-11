a7dda529717fcaf058c0fab1a524ed81d6e34567
George Schneeloch <gschneel@mit.edu>
2017-07-11 14:22:51 -0400

    Disable selenium test with intermittent failures (#3389)
    * Skip test for now
    
    * Fix skip

--- selenium_tests/basic_test.py
+++ selenium_tests/basic_test.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from factory import Iterator
 from factory.django import mute_signals
+import pytest
 from selenium.webdriver.common.keys import Keys
 
 from cms.factories import (
@@ -197,6 +198,7 @@ class LearnerTests(SeleniumTestsBase):
         )
 
 
+@pytest.mark.skip("Intermittent failures regarding patch and alert dialog")
 class ReviewFinancialAidTests(SeleniumTestsBase):
     """Look at the financial aid review page"""
 
