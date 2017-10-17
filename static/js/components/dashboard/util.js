2ebc90bd1479e75ea7bab2a88285c493179ab545
Alice Pote <alice.writes.wrongs@gmail.com>
2017-10-17 10:55:10 -0400

    Delete some unused code
    This removes some code that was left over from the dashboard redesign
    and inadvertantly not deleted.
    
    pr #3641

--- static/js/components/dashboard/util.js
+++ static/js/components/dashboard/util.js
@@ -1,22 +1,5 @@
 // @flow
-import moment from "moment"
 import _ from "lodash"
-
-export const isCurrentlyEnrollable = (
-  enrollmentStartDate: ?moment$Moment,
-  now: ?moment$Moment
-): boolean =>
-  enrollmentStartDate !== null &&
-  enrollmentStartDate !== undefined &&
-  enrollmentStartDate.isSameOrBefore(now || moment(), "day")
-
-export const isUpgradable = (
-  upgradeDeadlineDate: ?moment$Moment,
-  now: ?moment$Moment
-): boolean =>
-  upgradeDeadlineDate !== null &&
-  upgradeDeadlineDate !== undefined &&
-  upgradeDeadlineDate.isSameOrAfter(now || moment(), "day")
 
 export const formatGrade = (grade: ?number | ?string | null): string => {
   if (_.isNil(grade) || grade === "") {
