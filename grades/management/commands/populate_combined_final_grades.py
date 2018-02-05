900fcfa4f6cd83e4425dd9627e057bb987581ab4
annagav <annagav@mit.edu>
2018-02-05 10:12:46 -0500

    addressing comments

--- grades/management/commands/populate_combined_final_grades.py
+++ grades/management/commands/populate_combined_final_grades.py
@@ -1,3 +1,6 @@
+"""
+Find all passed exam grades and create CombinedFinalGrade if needed
+"""
 from django.core.management import BaseCommand
 
 from courses.models import Course
@@ -9,17 +12,17 @@ from micromasters.utils import now_in_utc
 class Command(BaseCommand):
     """
     Finds all passing exam grades for courses that have frozen runs
+    and creates CombinedFinalGrade records
     """
     help = "Finds all users that might need a CombinedFinalGrade"
 
     def handle(self, *args, **kwargs):  # pylint: disable=unused-argument
         courses = Course.objects.filter(
             program__live=True,
-            program__financial_aid_availability=True,
-            has_exam=True
+            program__financial_aid_availability=True
         )
         for course in courses:
-            if course.has_frozen_runs():
+            if course.has_frozen_runs() and course.has_exam:
                 exam_grades = ProctoredExamGrade.objects.filter(
                     course=course,
                     passed=True,
