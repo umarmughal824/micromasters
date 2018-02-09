fd8f17baf912c89ce78b703b9abb9de04f14967b
George Schneeloch <gschneel@mit.edu>
2018-02-09 16:06:38 -0500

    Update update_docker_hub.sh to use a new hash for each image (#3781)

--- travis/update-docker-hub.sh
+++ travis/update-docker-hub.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -eo pipefail
 
-docker build -t mitodl/mm_web_travis_next -f Dockerfile .
-docker build -t mitodl/mm_watch_travis -f travis/Dockerfile-travis-watch-build .
+export NEXT=$(date | md5sum | cut -c -6)
+export PROJECT_NAME="micromasters"
+echo "Next hash is $NEXT"
 
-docker push mitodl/mm_web_travis_next
-docker push mitodl/mm_watch_travis
+export WEB_IMAGE=mitodl/${PROJECT_NAME}_web_travis_${NEXT}
+export WATCH_IMAGE=mitodl/${PROJECT_NAME}_watch_travis_${NEXT}
+
+docker build -t $WEB_IMAGE -f Dockerfile .
+docker build -t $WATCH_IMAGE -f travis/Dockerfile-travis-watch-build .
+
+docker push $WEB_IMAGE
+docker push $WATCH_IMAGE
+
+sed -r -i "s|^FROM mitodl.+$|FROM $WEB_IMAGE|" travis/Dockerfile-travis-web
+sed -r -i "s|^FROM mitodl.+$|FROM $WATCH_IMAGE|" travis/Dockerfile-travis-watch
