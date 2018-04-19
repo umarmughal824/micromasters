786ed69531e9b96179dd3b36c63a66064594e8d7
Anna Gavrilman <annagav@users.noreply.github.com>
2018-04-19 11:11:42 -0400

    Fix migration (#3942)
    * Fix migration
    * Split migrations

--- grades/migrations/0014_remove_micromasterscoursecertificate_final_grade.py
+++ grades/migrations/0014_remove_micromasterscoursecertificate_final_grade.py
@@ -3,12 +3,13 @@
 from django.db import migrations
 from django.db.models import Count
 
-from dashboard.utils import get_mmtrack
+from micromasters.utils import generate_md5
 
 
 def delete_duplicate_cert(apps, schema_editor):
     Course = apps.get_model('courses', 'Course')
     MicromastersCourseCertificate = apps.get_model('grades', 'MicromastersCourseCertificate')
+    FinalGrade = apps.get_model('grades', 'FinalGrade')
 
     courses = Course.objects.filter(
         program__live=True,
@@ -20,17 +21,20 @@ def delete_duplicate_cert(apps, schema_editor):
             course=course
         ).values('user').annotate(Count('user')).filter(user__count__gt=1)
         for user in users_with_dup:
-            mmtrack = get_mmtrack(user['user'], course.program)
-            best_grade = mmtrack.get_best_final_grade_for_course(course)
+            best_grade = FinalGrade.objects.filter(
+                user=user['user'],
+                course_run__course=course,
+                passed=True
+            ).order_by('-grade').first()
             # trying to preserve the cert that is linked in the dashboard
             if MicromastersCourseCertificate.objects.filter(final_grade=best_grade).exists():
                 # delete other certificates
                 MicromastersCourseCertificate.objects.filter(
-                    course=course, user=user
+                    course=course, user=user['user']
                 ).exclude(final_grade=best_grade).delete()
             else:
                 certs = MicromastersCourseCertificate.objects.filter(
-                    course=course, user=user
+                    course=course, user=user['user']
                 )
                 certs.exclude(id__in=certs[:1]).delete()
 
@@ -38,15 +42,30 @@ def delete_duplicate_cert(apps, schema_editor):
 def add_final_grade(apps, schema_editor):
     MicromastersCourseCertificate = apps.get_model('grades', 'MicromastersCourseCertificate')
     FinalGrade = apps.get_model('grades', 'FinalGrade')
+    Course = apps.get_model('courses', 'Course')
 
-    certificates = MicromastersCourseCertificate.objects.filter(course__program__financial_aid_availability=True)
+    certificates= MicromastersCourseCertificate.objects.filter(course__isnull=True)
     for certificate in certificates:
-        certificate.final_grade = FinalGrade.objects.filter(
+        certificate.delete()
+    certificates = MicromastersCourseCertificate.objects.filter(course__program__financial_aid_availability=True)
+
+    for certificate in certificates:
+        passing_final_grades = FinalGrade.objects.filter(
             user=certificate.user,
             course_run__course=certificate.course,
             passed=True
-        ).order_by('-grade').first()
+        ).order_by('-grade')
+        certificate.final_grade = passing_final_grades.first()
         certificate.save()
+        # check if has other passing final_grades
+        for final_grade in passing_final_grades[1:]:
+            hash = generate_md5('{}|{}'.format(certificate.user_id, final_grade.course_run_id).encode('utf-8'))
+            MicromastersCourseCertificate.objects.create(
+                final_grade=final_grade,
+                user=final_grade.user,
+                course=certificate.course,
+                hash=hash
+            )
 
 
 class Migration(migrations.Migration):
@@ -56,9 +75,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(delete_duplicate_cert, add_final_grade),
-        migrations.RemoveField(
-            model_name='micromasterscoursecertificate',
-            name='final_grade',
-        ),
+                migrations.RunPython(delete_duplicate_cert, add_final_grade),
     ]
