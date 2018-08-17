76815d2a6df0d7f941789ed10c377c0a0d2d6b8a
Peter Pinch <pdpinch+github@hawklake.com>
2018-08-17 09:58:10 -0400

    improve program enrollments admin (#4099)
    - add search; 
    - use the raw id for user in program enrollments admin instead of drop-down

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -11,5 +11,10 @@ class ProgramEnrollmentAdmin(admin.ModelAdmin):
     """ModelAdmin for ProgramEnrollment"""
     list_display = ('user', 'program',)
     list_filter = ('program', 'program__live',)
+    raw_id_fields = ('user',)
+    search_fields = (
+        'user__username',
+        'user__email',
+    )
 
 admin.site.register(ProgramEnrollment, ProgramEnrollmentAdmin)
