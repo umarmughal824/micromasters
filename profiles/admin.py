16fa712e34b90a2beda368e5e5321c8e2974e03a
Peter Pinch <pdpinch+github@hawklake.com>
2018-09-11 09:08:40 -0400

    profile admin improvements (#4129)
    * add search and filter to profile admin list
    * use raw id instead of drop-down to avoid timeouts

--- profiles/admin.py
+++ profiles/admin.py
@@ -6,4 +6,26 @@ from django.contrib import admin
 
 from .models import Profile
 
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    """Admin for Profile"""
+    model = Profile
+    list_display = [
+        'user',
+        'first_name',
+        'last_name',
+        'email_optin',
+    ]
+    search_fields = [
+        'user__username',
+        'user__email',
+        'first_name',
+        'last_name',
+    ]
+    list_filter = [
+        'email_optin',
+        'user__is_active',
+    ]
+    raw_id_fields = ('user',)
+
+admin.site.register(Profile, ProfileAdmin)
