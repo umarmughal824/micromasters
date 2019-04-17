7096c1240f2abcabe42ce25f2f82a5704bd3cad4
Nathan Levesque <nlevesq@mit.edu>
2019-04-17 09:43:36 -0400

    Switched exam result auditing encryption to NaCl

--- exams/pearson/audit.py
+++ exams/pearson/audit.py
@@ -1,8 +1,6 @@
 """Exam auditing"""
-import base64
 import logging
 import os
-import re
 
 from boto.s3 import (
     connection,
@@ -10,28 +8,20 @@ from boto.s3 import (
 )
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from pretty_bad_protocol import gnupg
-import pretty_bad_protocol._util
+from nacl.encoding import Base64Encoder
+from nacl.public import PublicKey, SealedBox
 
 from micromasters import utils
 
 SSE_ENCRYPTION_ALGORITHM = 'AES256'
-REQUIRED_SETTINGS = [
-    'EXAMS_AUDIT_ENCRYPTION_FINGERPRINT',
-    'EXAMS_AUDIT_ENCRYPTION_PUBLIC_KEY',
-]
 
 log = logging.getLogger(__name__)
-
-# monkey patch gnupg so it doesn't utf-8 encode our files
-# see: https://github.com/isislovecruft/python-gnupg/issues/169
-pretty_bad_protocol._util.binary = lambda data: data  # pylint: disable=protected-access
 
 
 class S3AuditStorage:
     """Audit storage mechanism for S3"""
 
-    def configure(self):
+    def _validate(self):
         """
         Configures / validates the storage configuration
 
@@ -56,6 +46,7 @@ class S3AuditStorage:
         Returns:
             boto.s3.connection.S3Connection: a connection to S3
         """
+        self._validate()
         return connection.S3Connection(
             settings.EXAMS_AUDIT_AWS_ACCESS_KEY_ID,
             settings.EXAMS_AUDIT_AWS_SECRET_ACCESS_KEY
@@ -93,25 +84,53 @@ class S3AuditStorage:
             )
         )
 
-    def upload(self, filename, file_type):
+    def upload(self, filename, data, file_type):
         """
         Uploads the file to S3
 
         Args:
             filename (str): filename of the encrypted file
+            data (str): the encrypted data
             file_type (str): type of the encrypted file
 
         Returns:
             str: the key path in S3 where the file was stored
         """
         s3_key = self.get_s3_key(filename, file_type)
-        s3_key.set_contents_from_filename(
-            filename,
+        s3_key.set_contents_from_string(
+            data,
             headers={
                 'x-amz-server-side-encryption': SSE_ENCRYPTION_ALGORITHM,
             }
         )
         return s3_key.key
+
+
+def _get_public_key():
+    """
+    Get the configured PublicKey instance
+
+    Returns:
+        PublicKey:
+            the public key as configured in settings
+    """
+    if not settings.EXAMS_AUDIT_NACL_PUBLIC_KEY:
+        raise ImproperlyConfigured(
+            "EXAMS_AUDIT_NACL_PUBLIC_KEY is required but not set"
+        )
+
+    return PublicKey(settings.EXAMS_AUDIT_NACL_PUBLIC_KEY, encoder=Base64Encoder)
+
+
+def _get_sealed_box():
+    """
+    Get a NaCl SealedBox configured with the public key
+
+    Returns:
+        SealedBox:
+            the configured SealedBox
+    """
+    return SealedBox(_get_public_key())
 
 
 class ExamDataAuditor:
@@ -121,90 +140,25 @@ class ExamDataAuditor:
     REQUEST = 'request'
     RESPONSE = 'response'
 
-    def __init__(self, gpg=None, store=None):
-        self.gpg = gpg or gnupg.GPG()
+    def __init__(self, store=None):
         self.store = store or S3AuditStorage()
-        self.configured = False
-
-    @property
-    def fingerprint(self):
-        """The fingerprint for the public GPG key"""
-        return re.sub(r'\s+', '', settings.EXAMS_AUDIT_ENCRYPTION_FINGERPRINT)
-
-    @property
-    def public_key(self):
-        """The public GPG key"""
-        return base64.b64decode(settings.EXAMS_AUDIT_ENCRYPTION_PUBLIC_KEY).decode('utf-8')
-
-    def get_fingerprints(self):
-        """
-        Get the fingerprints of keys currently loaded on the system
-
-        Returns:
-            list: list of keys
-        """
-        return self.gpg.list_keys().fingerprints
-
-    def configure(self):
-        """
-        Performs a one-time configuration of GPG keys
-
-        Raises:
-            ImproperlyConfigured: settings are missing or invalid
-
-        Returns:
-            bool: True if configuration successful
-        """
-        if self.configured:
-            return
-
-        # if any of the settings are not set, we consider this feature not enabled
-        if not all(getattr(settings, key) for key in REQUIRED_SETTINGS):
-            raise ImproperlyConfigured(
-                "One of EXAMS_AUDIT_ENCRYPTION_PUBLIC_KEY or "
-                "EXAMS_AUDIT_ENCRYPTION_FINGERPRINT is required but not set"
-            )
-
-        if self.fingerprint in self.get_fingerprints():
-            self.configured = True
-            return
-
-        result = self.gpg.import_keys(self.public_key)
-
-        if result.counts['not_imported'] != 0:
-            raise ImproperlyConfigured('Error importing GPG keys for exam auditing: {}'.format(result.summary()))
-
-        if self.fingerprint not in result.fingerprints:
-            raise ImproperlyConfigured(
-                "Exam auditing GPG key successfully imported but {actual} "
-                "does not contain fingerprint: {expected}".format(
-                    actual=result.fingerprints,
-                    expected=self.fingerprint,
-                )
-            )
-
-        self.store.configure()
-
-        self.configured = True
 
     def encrypt(self, filename, encrypted_filename):
         """
-        Encrypts the local file with GPG
+        Encrypts the local file
 
         Args:
             filename (str): absolute path to the local file
             encrypted_filename (str): absolute path to the local encrypted file
+
+        Returns:
+            str:
+                the encrypted data
         """
         log.debug('Encrypting file %s to %s', filename, encrypted_filename)
 
         with open(filename, 'rb') as source_file:
-            self.gpg.encrypt(
-                source_file,
-                self.fingerprint,
-                armor=False,
-                symmetric=False,
-                output=encrypted_filename
-            )
+            return _get_sealed_box().encrypt(source_file.read())
 
     def upload_encrypted_file(self, filename, file_type):
         """
@@ -217,11 +171,11 @@ class ExamDataAuditor:
         Returns:
             str: path to the stored file
         """
-        encrypted_filename = '{}.gpg'.format(filename)
+        encrypted_filename = '{}.nacl'.format(filename)
         try:
-            self.encrypt(filename, encrypted_filename)
+            encrypted_data = self.encrypt(filename, encrypted_filename)
 
-            return self.store.upload(encrypted_filename, file_type)
+            return self.store.upload(encrypted_filename, encrypted_data, file_type)
         finally:
             utils.safely_remove_file(encrypted_filename)
 
@@ -238,8 +192,6 @@ class ExamDataAuditor:
         """
         if not settings.EXAMS_AUDIT_ENABLED:
             return None
-
-        self.configure()
 
         return self.upload_encrypted_file(filename, file_type)
 
--- exams/pearson/audit_test.py
+++ exams/pearson/audit_test.py
@@ -4,53 +4,22 @@ from unittest.mock import (
     DEFAULT,
     Mock
 )
-import base64
 import copy
 import tempfile
-import os
-import re
 
 from django.core.exceptions import ImproperlyConfigured
 from django.test import override_settings
-from pretty_bad_protocol import gnupg
+from nacl.public import PrivateKey, SealedBox
+from nacl.encoding import Base64Encoder
 import pytest
 
 from exams.pearson import audit
 
-# pylint: disable=missing-docstring,redefined-outer-name,unused-argument
-
-PUBLIC_KEY = """-----BEGIN PGP PUBLIC KEY BLOCK-----
-Version: GnuPG v1
-
-mI0EWN1k1wEEALiMsgDtV68qvg4d07eY6LrG8L7PWdSgZGon+UUMIMHQXe1CpGbn
-Cgm8it/gN8y/oR8TUUs3zzxndbxERQOHb5wi8zs609dMK9xZQqhuCLpbv2Uo0A1p
-qDoWLcxtcNnN3fbBWEJNTyFlwd90pLbDjmp7Hb6+PAHnyWDIJR4RWRTdABEBAAG0
-IkhlaW56IERvb2ZlbnNobWlydHogPGRvb2ZAbWl0LmVkdT6IuAQTAQIAIgUCWN1k
-1wIbAwYLCQgHAwIGFQgCCQoLBBYCAwECHgECF4AACgkQKpcMy6vPbHRW0wP/cvr5
-5av7I82hQkHVmipVnjj9VtBDW5x4iTvYqzQCKPvKhfG7bw3VObrQaV+iUoWBzr0s
-iSjKDKUFcrwrjnjbIrWHfKEtcG+wDcnhWfLCnSyP8IPavgdfDIWEE7/tpSmA8wvp
-inh3YnAicgF487TEpRxy25I1iJDpx8P0GWbms1u4jQRY3WTXAQQAvmfcnbUZkkW4
-AasgBI9oxfkh7chOiHHtaIjmTGdo1sOLyvQBQhIgV9SiN/iTc57VvJ/svWs/bh4q
-BzsXQFpI/WQQUaj5b1y1A8geYsVLUR7eFFtFb+/ujBdzX43QmjPiDCikpltfZJOa
-AUtLMpfrHKmvrULQG1Nrn3x97MmMW3UAEQEAAYifBBgBAgAJBQJY3WTXAhsMAAoJ
-ECqXDMurz2x0YWYEALCoRIjJqomOk1G5AZxJl5zRhG++HXBnh86NtJCiQ530wHBQ
-lZzh+Qa/ZqOJ4/bucvr57pTekxaPbQeocPkiKPTRHGR+pCnQ+v+sy5ZDuSVwzqtX
-j6VMC+4fsNKKbrSgFBTgOViYgaa2jkIy3b4xfG9YPVT6O7hfOgmXHDs2/e7f
-=RI1Z
------END PGP PUBLIC KEY BLOCK-----"""
-FINGERPRINT = "8690 9C7E 0929 58F8 DEBE  9365 2A97 0CCB ABCF 6C74"
-FINGERPRINT_STRIPPED = re.sub(r'\s+', '', FINGERPRINT)
-
-
-def encode_test_value(value):
-    """Encode a test value"""
-    return base64.b64encode(bytes(value, 'utf-8'))
+# pylint: disable=missing-docstring,redefined-outer-name
 
 
 DEFAULT_SETTINGS = {
     'EXAMS_AUDIT_ENABLED': True,
-    'EXAMS_AUDIT_ENCRYPTION_PUBLIC_KEY': encode_test_value(PUBLIC_KEY),
-    'EXAMS_AUDIT_ENCRYPTION_FINGERPRINT': FINGERPRINT,
     'EXAMS_AUDIT_S3_BUCKET': '.test.bucket.name.',
     'EXAMS_AUDIT_AWS_ACCESS_KEY_ID': 'test.id',
     'EXAMS_AUDIT_AWS_SECRET_ACCESS_KEY': 'test.access.key',
@@ -58,67 +27,63 @@ DEFAULT_SETTINGS = {
 
 
 @pytest.fixture()
-def valid_settings():
+def private_key():
+    """Creates a new NaCl private key"""
+    return PrivateKey.generate()
+
+
+@pytest.fixture()
+def valid_settings(private_key):
     """
     Fixture that provides valid (passes checks in configure()) configuration
     """
-    with override_settings(**DEFAULT_SETTINGS):
+    settings = copy.copy(DEFAULT_SETTINGS)
+    settings.update({
+        'EXAMS_AUDIT_NACL_PUBLIC_KEY': Base64Encoder.encode(bytes(private_key.public_key)),
+    })
+
+    with override_settings(**settings):
         yield DEFAULT_SETTINGS
 
 
-@pytest.fixture(
-    ids=['both_bad', 'pubkey_bad', 'fingerprint_bad'],
-    params=[{
-        'EXAMS_AUDIT_ENCRYPTION_FINGERPRINT': 'bad',
-        'EXAMS_AUDIT_ENCRYPTION_PUBLIC_KEY': encode_test_value('bad'),
-    }, {
-        'EXAMS_AUDIT_ENCRYPTION_PUBLIC_KEY': encode_test_value('bad'),
-    }, {
-        'EXAMS_AUDIT_ENCRYPTION_FINGERPRINT': 'bad',
-    }]
-)
-def invalid_settings(request):
+@pytest.fixture()
+def invalid_settings():
     """
     Fixture that runs a test against a set of invalid configurations
     """
     settings = copy.copy(DEFAULT_SETTINGS)
-    settings.update(request.param)
-
-    with override_settings(**settings):
-        yield settings
-
-
-@pytest.fixture(
-    params=[
-        'EXAMS_AUDIT_ENCRYPTION_FINGERPRINT',
-        'EXAMS_AUDIT_ENCRYPTION_PUBLIC_KEY',
-    ]
-)
-def missing_settings(request):
-    """
-    Fixture that runs a test with each of the specified settings keys set to None
-    """
-    settings = copy.copy(DEFAULT_SETTINGS)
-    settings[request.param] = None
+    settings.update({
+        'EXAMS_AUDIT_NACL_PUBLIC_KEY': Base64Encoder.encode('bad'),
+    })
 
     with override_settings(**settings):
         yield settings
 
 
 @pytest.fixture()
-def s3_store(valid_settings):
+def missing_settings():
+    """
+    Fixture that runs a test with each of the specified settings keys set to None
+    """
+    settings = copy.copy(DEFAULT_SETTINGS)
+    settings["EXAMS_AUDIT_NACL_PUBLIC_KEY"] = None
+
+    with override_settings(**settings):
+        yield settings
+
+
+@pytest.fixture()
+def s3_store(valid_settings):  # pylint: disable=unused-argument
     """S3 storage for tests"""
     return audit.S3AuditStorage()
 
 
 @pytest.fixture
-def auditor(valid_settings):
+def auditor(valid_settings):  # pylint: disable=unused-argument
     """
     ExamDataAuditor that cleans up the GPG keys after itself
     """
-    gpg = gnupg.GPG()
-    yield audit.ExamDataAuditor(gpg=gpg, store=Mock(spec=audit.S3AuditStorage))
-    gpg.delete_keys(FINGERPRINT_STRIPPED)
+    yield audit.ExamDataAuditor(store=Mock(spec=audit.S3AuditStorage))
 
 
 def test_s3_store_get_connection_valid(s3_store, mocker):
@@ -129,16 +94,17 @@ def test_s3_store_get_connection_valid(s3_store, mocker):
 
 
 @pytest.mark.parametrize('key', [
+    'EXAMS_AUDIT_S3_BUCKET',
     'EXAMS_AUDIT_AWS_ACCESS_KEY_ID',
     'EXAMS_AUDIT_AWS_SECRET_ACCESS_KEY',
 ])
-def test_s3_store_configure(s3_store, key):
+def test_s3_store_missing_settings(s3_store, key):
     """Test configure() against missing settings"""
     with override_settings(**{
         key: None,
     }):
         with pytest.raises(ImproperlyConfigured):
-            s3_store.configure()
+            s3_store.upload('filename', 'data', 'filetype')
 
 
 def test_s3_store_get_s3_key(s3_store, mocker):
@@ -154,126 +120,60 @@ def test_exam_data_s3_store_upload(s3_store, mocker):
     """Test that the S3 store uploads a file with the correct headers"""
     mocker.patch.object(s3_store, 'get_s3_key')
 
-    s3_store.upload('filename', 'filetype')
+    s3_store.upload('filename', 'data', 'filetype')
     mock_key = s3_store.get_s3_key.return_value
 
-    assert mock_key.set_contents_from_filename.call_count == 1
-    assert mock_key.set_contents_from_filename.call_args == call(
-        'filename',
+    assert mock_key.set_contents_from_string.call_count == 1
+    assert mock_key.set_contents_from_string.call_args == call(
+        'data',
         headers={
             'x-amz-server-side-encryption': 'AES256'
         }
     )
 
 
-def test_exam_data_auditor_fingerprint(auditor):
-    """Test that fingerprint strips whitespace from the key"""
-    assert auditor.fingerprint == FINGERPRINT_STRIPPED
-
-
-def test_exam_data_auditor_public_key(auditor):
-    """Test that this decodes a base64 encoded pubkey"""
-    assert auditor.public_key == PUBLIC_KEY
-
-
-def test_exam_data_auditor_configure(auditor):
-    """Test that configure() succeeds with correct settings"""
-    auditor.configure()
-    assert FINGERPRINT_STRIPPED in auditor.gpg.list_keys().fingerprints
-    assert auditor.configured is True
-
-
-def test_exam_data_auditor_configure_already_configured(auditor, mocker):
-    """Test that configure() succeeds if already configured"""
-    mocker.patch.object(auditor, 'get_fingerprints', return_value=[FINGERPRINT_STRIPPED])
-    mocker.spy(auditor.gpg, 'import_keys')
-    auditor.configure()
-    assert auditor.gpg.import_keys.call_count == 0
-    assert auditor.configured is True
-
-
-def test_exam_data_auditor_enabled(auditor, missing_settings, mocker):
+@pytest.mark.usefixtures('missing_settings')
+@pytest.mark.parametrize('is_enabled', [True, False])
+def test_exam_data_auditor_enabled(auditor, mocker, is_enabled):
     """Test that audit_file() respected the enabled flag"""
-    mocker.patch.multiple(auditor, configure=DEFAULT, upload_encrypted_file=DEFAULT)
-    # auditor is disabled
-    with override_settings(**{
-        'EXAMS_AUDIT_ENABLED': False,
-    }):
-        auditor.audit_file('file', 'filetype')
-    assert auditor.configure.call_count == 0
-    # auditor is enabled
-    with override_settings(**{
-        'EXAMS_AUDIT_ENABLED': True,
-    }):
-        auditor.audit_file('file', 'filetype')
-    assert auditor.configure.call_count == 1
+    mocker.patch.multiple(auditor, encrypt=DEFAULT, upload_encrypted_file=DEFAULT)
+    with tempfile.NamedTemporaryFile() as audit_file:
+        with override_settings(**{
+            'EXAMS_AUDIT_ENABLED': is_enabled,
+        }):
+            auditor.audit_file(audit_file.name, 'filetype')
+        assert auditor.upload_encrypted_file.call_count == (1 if is_enabled else 0)
 
 
-def test_exam_data_auditor_configure_missing_settings(auditor, missing_settings, mocker):
+@pytest.mark.usefixtures('missing_settings')
+def test_exam_data_auditor_configure_missing_settings(auditor):
     """Test that configure() fails with missing settings"""
-    mocker.spy(auditor, 'get_fingerprints')
-    with pytest.raises(ImproperlyConfigured):
-        auditor.configure()
-    assert auditor.get_fingerprints.call_count == 0
-    assert auditor.configured is False
+    with tempfile.NamedTemporaryFile() as audit_file:
+        with pytest.raises(ImproperlyConfigured):
+            auditor.audit_file(audit_file.name, 'filetype')
 
 
-def test_exam_data_auditor_configure_bad_settings(auditor, invalid_settings):
-    """Test that configure() fails with invalid settings"""
-    with pytest.raises(ImproperlyConfigured):
-        auditor.configure()
-    assert auditor.configured is False
-
-
-def test_exam_data_auditor_configure_not_imported(auditor, mocker):
-    """Test that configure() succeeds if already configured"""
-    mock = mocker.patch.object(auditor.gpg, 'import_keys')
-    mock.return_value.counts = {
-        'not_imported': 1,
-    }
-    with pytest.raises(ImproperlyConfigured):
-        auditor.configure()
-    assert auditor.configured is False
-
-
-def test_exam_data_auditor_audit_file(auditor):
+def test_exam_data_auditor_audit_file(auditor, private_key):
     """Test that the auditor encrypts and uploads the file"""
     with tempfile.NamedTemporaryFile() as audit_file:
         # the auditor encrypts the file to a local path
-        expected_encrypted_filename = '{}.gpg'.format(audit_file.name)
+        expected_encrypted_filename = '{}.nacl'.format(audit_file.name)
         expected_encrypted_keypath = 'keypath'  # computed inside _upload_to_s3
+        file_contents = b'unencrypted file contents'
 
-        def upload_side_effect(encrypted_filename, file_type):
+        audit_file.write(file_contents)
+        audit_file.flush()
+
+        def upload_side_effect(encrypted_filename, encrypted_data, file_type):
             """Verify the encrypted file exists at this point"""
             # verify the upload was triggered with the expected encrypted filename and that file exists
+            assert SealedBox(private_key).decrypt(encrypted_data) == file_contents
             assert encrypted_filename == expected_encrypted_filename
-            assert os.path.exists(encrypted_filename) is True
             assert file_type == 'sometype'
             return expected_encrypted_keypath
         auditor.store.upload.side_effect = upload_side_effect
 
         assert auditor.audit_file(audit_file.name, 'sometype') == expected_encrypted_keypath
-
-        # verify it cleaned up after itself
-        assert os.path.exists(expected_encrypted_filename) is False
-
-
-def test_exam_data_auditor_audit_upload_error(auditor):
-    with tempfile.NamedTemporaryFile() as audit_file:
-        expected_encrypted_filename = '{}.gpg'.format(audit_file.name)
-        auditor.store.upload.side_effect = IOError()
-        with pytest.raises(IOError):
-            auditor.audit_file(audit_file.name, 'sometype')
-        assert os.path.exists(expected_encrypted_filename) is False
-
-
-def test_exam_data_auditor_audit_encrypt_error(auditor, mocker):
-    with tempfile.NamedTemporaryFile() as audit_file:
-        expected_encrypted_filename = '{}.gpg'.format(audit_file.name)
-        mocker.patch.object(auditor.gpg, 'encrypt', side_effect=IOError())
-        with pytest.raises(IOError):
-            auditor.audit_file(audit_file.name, 'sometype')
-        assert os.path.exists(expected_encrypted_filename) is False
 
 
 def test_exam_data_auditor_audit_request_file(auditor, mocker):
