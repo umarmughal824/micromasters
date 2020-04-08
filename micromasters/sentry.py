71c0737a990dce20978c0ff1462fd2e1721b989d
Umar Asghar <umar.asghar@arbisoft.com>
2020-04-08 11:30:27 +0500

    Configure sentry's python sdk to not capture SystemExit

--- micromasters/sentry.py
+++ micromasters/sentry.py
@@ -1,8 +1,12 @@
 """Sentry setup and configuration"""
+from celery.exceptions import WorkerLostError
 import sentry_sdk
 from sentry_sdk.integrations.celery import CeleryIntegration
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
+
+# these errors occur when a shutdown is happening (usually caused by a SIGTERM)
+SHUTDOWN_ERRORS = (WorkerLostError, SystemExit)
 
 
 def init_sentry(*, dsn, environment, version, log_level):
@@ -14,10 +18,28 @@ def init_sentry(*, dsn, environment, version, log_level):
         version (str): the version of the application
         log_level (str): the sentry log level
     """
+
+    def before_send(event, hint):
+        """
+        Filter or transform events before they're sent to Sentry
+        Args:
+            event (dict): event object
+            hint (dict): event hint, see https://docs.sentry.io/platforms/python/#hints
+        Returns:
+            dict or None: returns the modified event or None to filter out the event
+        """
+        if 'exc_info' in hint:
+            _, exc_value, _ = hint['exc_info']
+            if isinstance(exc_value, SHUTDOWN_ERRORS):
+                # we don't want to report shutdown errors to sentry
+                return None
+        return event
+
     sentry_sdk.init(
         dsn=dsn,
         environment=environment,
         release=version,
+        before_send=before_send,
         integrations=[
             DjangoIntegration(),
             CeleryIntegration(),
